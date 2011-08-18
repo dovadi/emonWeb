e7544de050c3fb34d88d6dd50bff511afb791eb9
dovadi <frank.oxener@gmail.com>
2011-08-18 15:53:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emonweb::Application.config.secret_token = '1c9fb4c7a2a999a2dcfed41cf73d7fe1955c24e51ac8fb039524703e45cb5071de006f3ddd5bdde4bcdc4a91589fa06403af8aa1bf9db6dd72b986528e2fd232'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Emonweb::Application.config.session_store :cookie_store, key: '_emonweb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Emonweb::Application.config.session_store :active_record_store
