d39f3600f91c8e472f54320f9016cac7de1cd79e
dovadi <frank.oxener@gmail.com>
2011-09-15 21:45:22 +0200

    Add admin role to an user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110915192145) do
+ActiveRecord::Schema.define(:version => 20110915194152) do
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(:version => 20110915192145) do
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20110915192145) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                                 :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
