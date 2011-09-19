88a878e086e53e29f5906b1b9a0f5ac1c7d7116f
dovadi <frank.oxener@gmail.com>
2011-09-19 21:28:40 +0200

    Added acceptance spec for creating and deleting authentication token

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,8 @@ Emonweb::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  HOST = 'localhost:3000'
+
+  config.action_mailer.default_url_options = { :host => HOST }
 end
---
+++ spec/acceptance/create_and_destroy_authication_token_spec.rb
@@ -0,0 +1,33 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Create and destroy authentication token', %q{
+  In order to provide web access for my monitoring device
+  As a user
+  I want to create and destroy an authentication token
+} do
+
+  background do
+    @user = Factory(:user)
+    sign_in_as @user
+  end
+
+  scenario 'Create authentication token' do
+    click_link 'Edit account'
+    page.should have_content('Token Empty')
+    click_link 'Generate Token'
+    @user.reload
+    page.should have_content(@user.authentication_token)
+    page.should have_content('You can use this url to login')
+    page.should have_content(root_path(:auth_token => @user.authentication_token))
+  end
+
+  scenario 'Destroy authentication token' do
+    @user.reset_authentication_token!
+    @user.reload
+    click_link 'Edit account'
+    page.should have_content(@user.authentication_token)
+    click_link 'Delete Token'
+    page.should have_content('Token Empty')
+  end
+
+end
