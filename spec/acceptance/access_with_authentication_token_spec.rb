90a565c5d564772e3b890b7f8179b88e4f0aed82
dovadi <frank.oxener@gmail.com>
2011-09-19 21:35:23 +0200

    Added acceptance spec for access with an authentication token

---
+++ spec/acceptance/access_with_authentication_token_spec.rb
@@ -0,0 +1,22 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Access with authentication token', %q{
+  In order to post data with a monitoring device
+  As a device
+  I want to have access with an authentication token
+} do
+  
+  background do
+    @user = Factory(:user)
+    @user.reset_authentication_token!
+    @user.reload
+  end
+
+  scenario 'Get access with an authentication token' do
+    visit root_path(:auth_token => @user.authentication_token)
+    within '.content' do
+      page.should have_content("Hello #{@user.email}")
+    end
+  end
+
+end
