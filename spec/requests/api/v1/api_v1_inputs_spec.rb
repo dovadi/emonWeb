182b4acdd1e1bb75209a16cbb0db081b2a7c9c01
dovadi <frank.oxener@gmail.com>
2011-10-11 22:23:25 +0200

    WORK IN PROGRESS: verify posting to api

--- spec/requests/api/v1/api_v1_inputs_spec.rb
+++ spec/requests/api/v1/api_v1_inputs_spec.rb
@@ -15,4 +15,15 @@ describe "Api::V1::Inputs" do
       response.status.should be(200)
     end
   end
+
+  describe 'POST /api' do
+    it 'create new inputs' do
+      expect do
+        post api_path(:auth_token => @user.authentication_token, :water => 12.35, :solar => 48.23)
+      end.to change(Input, :count).by(2)
+      response.status.should be(302)
+      Input.last.user_id.should be(@user.id)
+    end
+  end
+
 end
