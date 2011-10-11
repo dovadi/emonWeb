d31f5d86407857ce7f5e80ffbbb07d9521dfd2e0
dovadi <frank.oxener@gmail.com>
2011-10-11 21:57:56 +0200

    Implemented create_or_update for Input

--- app/models/input.rb
+++ app/models/input.rb
@@ -1,4 +1,24 @@
+class NoUserIdGiven < Exception; end
+
 class Input < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name, :last_value
+  validates_uniqueness_of :name, :scope => :user_id
+
+  def self.create_or_update(attributes)
+    attributes.symbolize_keys!
+    raise NoUserIdGiven unless attributes.keys.include?(:user_id)
+    attributes.delete(:controller)
+    attributes.delete(:action)
+    user_id = attributes.delete(:user_id)
+    attributes.each do |key, value|
+      existing_input = find_by_name_and_user_id(key.to_s, user_id)
+      if existing_input
+        existing_input.touch(:updated_at) if existing_input.last_value == value
+        existing_input.update_attribute(:last_value, value)
+      else
+        create!(:name => key.to_s, :last_value => value)
+      end
+    end
+  end
 end
--- spec/models/input_spec.rb
+++ spec/models/input_spec.rb
@@ -4,17 +4,70 @@ describe Input do
 
   before(:each) do
     @attr = { 
-      :name => 'power',
-      :last_value => 252.4,
+      :name       => 'water',
+      :last_value => 252.4
     }
   end
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :last_value }
+  it { should belong_to :user }
+
+  it 'should only accept an unique name' do
+    Input.create!(@attr)
+    Input.new.should validate_uniqueness_of(:name).scoped_to(:user_id)
+  end
 
   it 'should create a new instance given a valid attribute' do
     Input.create!(@attr)
   end
 
-  it { should belong_to :user }
+  describe 'Create or Update' do
+    before(:each) do
+      @input_attrs ={
+        :water   => 20.45,
+        :solar   => 12.34,
+        :user_id => 100
+      }
+    end
+
+    it 'should raise an exception if user_id is not given' do
+      expect do
+        Input.create_or_update(:heat => 56)
+      end.to raise_error NoUserIdGiven
+    end
+
+    it 'should create or update an input based on the given attributes' do
+      expect do
+        Input.create_or_update(@input_attrs)
+      end.to change(Input, :count).by(2)
+    end
+
+    it 'should create or update an input based on the given attributes but ignore controller and action keys' do
+      expect do
+        Input.create_or_update(@input_attrs.merge(:controller => 'inputs', :action => 'create'))
+      end.to change(Input, :count).by(2)
+    end
+
+    it 'should only create the input that does not exist yet' do
+      Input.create!(@attr.merge(:user_id => 100))
+      expect do
+        Input.create_or_update(@input_attrs)
+      end.to change(Input, :count).by(1)
+    end
+
+    it 'should update the last value if input already exists' do
+      input = Input.create!(@attr.merge(:user_id => 100))
+      Input.create_or_update(@input_attrs)
+      input.reload
+      input.last_value.should == 20.45
+    end
+
+    it 'should update the last value if input already exists and the last value is the same' do
+      input = Input.create!(@attr.merge(:user_id => 100))
+      Input.any_instance.expects(:touch).with(:updated_at)
+      Input.create_or_update(:water => 252.4, :user_id => 100)
+    end
+  end
+
 end
