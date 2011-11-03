30e6ad8c2827449029ad67f4d3ae2ab9d9002edd
dovadi <frank.oxener@gmail.com>
2011-11-03 14:51:42 +0100

    Divide specs in processing for separate and serial

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -18,7 +18,28 @@ describe Feed do
     @feed.should be_valid
   end
 
-  describe 'Process of data' do
+  describe 'Process of data in case of two seperate processor' do
+    before(:each) do
+      @attr.merge!(:processors => [{:multiply => [2]}, {:divide => [3]}])
+      @processor_klass = mock
+      String.any_instance.expects(:constantize).twice.returns(@processor_klass)
+    end
+
+    it 'should perform the given processors' do
+      multiply_processor = mock
+      multiply_processor.stubs(:perform).returns(500)
+      divide_processor = mock
+      divide_processor.stubs(:perform).returns(123.45)
+
+      @processor_klass.expects(:new).with(252.55, [2]).returns(multiply_processor)
+      @processor_klass.expects(:new).with(252.55, [3]).returns(divide_processor)
+      Feed.create!(@attr)
+      Feed.last.processed_value_0.should == 500
+      Feed.last.processed_value_1.should == 123.45
+    end
+  end
+
+  describe 'Process of data in case of two processor in serial' do
     before(:each) do
       @attr.merge!(:processors => [{:multiply => [2], :divide => [3]}])
       @processor_klass = mock
