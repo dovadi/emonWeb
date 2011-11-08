98c5896e3d9fb9a91cfb857877cb8fcb0c414619
dovadi <frank.oxener@gmail.com>
2011-11-08 17:57:03 +0100

    Forgot to remove more unused code

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,8 +2,6 @@ class UndefinedProcessor < Exception; end
 
 class Feed < ActiveRecord::Base
 
-  before_save :process_data, :unless => Proc.new { |feed| feed.processors.nil? }
-
   before_create :set_corresponding_table_name
   after_create  :reset_corresponding_table_name
 
@@ -40,22 +38,6 @@ class Feed < ActiveRecord::Base
 
   def reset_corresponding_table_name
     Feed.table_name = 'feeds'
-  end
-
-  def process_data
-    processors.each_with_index do |processor, index|
-      processed_value = value
-      processor.each do |name, args|
-        begin
-          processor_name = (name.to_s.capitalize + 'Processor')
-          processor = processor_name.constantize.new(processed_value, args)
-        rescue NameError => e
-          raise UndefinedProcessor, "Undefined processor #{processor_name}"
-        end
-        processed_value = processor.perform
-      end
-      self.send('processed_value_' + index.to_s + '=', processed_value)
-    end
   end
 
 end
