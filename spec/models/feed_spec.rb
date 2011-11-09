28f245143e75d94bbc04ec2234fb4f9a4b58e3a9
dovadi <frank.oxener@gmail.com>
2011-11-09 17:03:35 +0100

    Process data after update of a feed implemented

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -9,4 +9,45 @@ describe Feed do
 
   it { should belong_to :input}
 
+  describe 'Processing of data' do  
+
+    before(:each) do
+      @attr = {
+        :last_value => 252.55,
+        :user_id    => 100,
+        :input_id   => 1,
+        :processors => [[:scale, 1.23], [:offset, 3.5]]
+      }
+      @processor_klass = mock
+      String.any_instance.expects(:constantize).twice.returns(@processor_klass)
+    end
+
+    it 'should perform the given processors' do
+      scale_processor = mock
+      scale_processor.stubs(:perform).returns(500)
+      offset_processor = mock
+      offset_processor.stubs(:perform).returns(1546.34)
+      @processor_klass.expects(:new).with(252.55, 1.23).returns(scale_processor)
+      @processor_klass.expects(:new).with(500, 3.5).returns(offset_processor)
+      Feed.create!(@attr)
+      Feed.last.last_value.should == 1546.34
+    end
+  end
+
+  describe 'Process of data with an undefined processor' do
+    before(:each) do
+      @attr = {
+        :last_value => 252.55,
+        :user_id    => 100,
+        :input_id   => 1,
+        :processors => [[:unknown, 3.5]]
+      }
+    end
+    it 'should raise an UndefinedProcessor expection' do
+      expect do
+        Feed.create!(@attr)
+      end.to raise_error UndefinedProcessor
+    end
+  end
+
 end
