4971519d3c60ac28ea41415ee153ca7f7c98546d
dovadi <frank.oxener@gmail.com>
2011-12-04 15:10:22 +0100

    Oops forgot to add the widgets controller

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -4,7 +4,7 @@ class WidgetsController < ApplicationController
     begin
       feed = current_user.feeds.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-      feed = current_user.feeds.first
+      feed = current_user.feeds.order(:id).first
     end
     @id = feed.id if feed
   end
