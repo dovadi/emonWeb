40899ce1b86448e4e5d5cd291604f1fa6f5a3dec
dovadi <frank.oxener@gmail.com>
2011-12-24 16:17:58 +0100

    Refactor DataAverage, now with a Calculator for the next timeslot

--- app/models/data_average.rb
+++ app/models/data_average.rb
@@ -1,30 +1,16 @@
 class DataAverage
 
   def self.calculate!(identified_by, timeslot)
+    return nil unless timeslot.present?
     last  = DataStore.from(identified_by, timeslot).last
     if last
       value = DataStore.from(identified_by).where('created_at >= ? ',last.created_at).average(:value)
     else
       value = DataStore.from(identified_by).average(:value)
     end
-    DataStore.create(:value => value, :identified_by => identified_by, :timeslot => timeslot)
-    calculate_next_average(identified_by, timeslot, last) if last
-  end
-
-  private
-
-  def self.calculate_next_average(identified_by, timeslot, last)
-    case timeslot
-    when :one_min
-      self.calculate!(identified_by, :five_mins)    if last.created_at.min % 5   == 0
-    when :five_mins
-      self.calculate!(identified_by, :fifteen_mins) if last.created_at.min % 15   < 5
-    when :fifteen_mins
-      self.calculate!(identified_by, :one_hour)     if last.created_at.min        < 15
-    when :one_hour
-      self.calculate!(identified_by, :four_hours)   if last.created_at.hour % 4  == 0
-    when :four_hours
-      self.calculate!(identified_by, :twelve_hours) if last.created_at.hour % 12 == 0
+    unless value.nil?
+      ds = DataStore.create!(:value => value, :identified_by => identified_by, :timeslot => timeslot)
+      calculate!(identified_by, Calculator.next(timeslot, ds.created_at))
     end
   end
 
--- spec/models/data_average_spec.rb
+++ spec/models/data_average_spec.rb
@@ -3,32 +3,45 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe DataAverage do
 
   describe 'DataAverage calculate! average' do
-     before(:each) do
-       @input = Input.new
-       @input.create_data_store_tables('data_store_789')
-     end
+    before(:each) do
+      drop_data_stores
+      @input = Input.new
+      @input.create_data_store_tables('data_store_789')
+     
+      DataStore.create(:value => 1, :identified_by => 789)
+      DataStore.create(:value => 2, :identified_by => 789)
+    end
 
-     it 'should calculate average! (one_min)' do
-       DataStore.create(:value => 1, :identified_by => 789)
-       DataStore.create(:value => 2, :identified_by => 789)
+    it 'should do nothing if timeslot is nil' do
+      DataAverage.calculate!(789, nil).should be_nil
+    end
 
-       DataAverage.calculate!(789, :one_min)
-       data = DataStore.from(789, :one_min).last
-       data.value.should == 1.5
-     end
+    it 'should calculate average! (one_min)' do
+      Calculator.expects(:next).with(:one_min, anything).returns(nil)
+      DataAverage.calculate!(789, :one_min)
+      data = DataStore.from(789, :one_min).last
+      data.value.should == 1.5
+    end
 
-     it 'should calculate average for every other timeslot' do
-       DataStore.create(:value => 1, :identified_by => 789)
-       DataStore.create(:value => 2, :identified_by => 789)
+    it 'should calculate average for every other timeslot' do
+      Calculator.expects(:next).with(:four_hours, anything).returns(nil)
+      DataAverage.calculate!(789, :four_hours)
+      data = DataStore.from(789, :four_hours).last
+      data.value.should == 1.5
+    end
 
-       DataAverage.calculate!(789, :one_hour)
-       data = DataStore.from(789, :one_hour).last
-       data.value.should == 1.5
-     end
+    it 'should calculate average! (one_min) if there is already another record stored' do
+      DataStore.create(:value => 4.5, :identified_by => 789, :timeslot => :one_min)
+      DataStore.any_instance.stubs(:created_at).returns(Time.now - 1.hour) #force calculating an average
+      Calculator.expects(:next).with(:one_min, anything).returns(nil)
+      DataAverage.calculate!(789, :one_min)
+      data = DataStore.from(789, :one_min).last
+      data.value.should == 1.5
+    end
 
-     after(:each) do
-       drop_data_stores
-     end
-   end
+    after(:each) do
+      drop_data_stores
+    end
+  end
 
 end
