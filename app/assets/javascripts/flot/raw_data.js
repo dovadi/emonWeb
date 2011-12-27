2439084483d7b6cd3f98916a7d3849f0263a6322
dovadi <frank.oxener@gmail.com>
2011-12-27 15:46:52 +0100

    Refactor javascript library for drawing raw graph

--- app/assets/javascripts/flot/raw_data.js
+++ app/assets/javascripts/flot/raw_data.js
@@ -1,62 +1,88 @@
-//--------------------------------------------------------------------------------------
-// Plot flot graph
-//--------------------------------------------------------------------------------------
-function plotGraph(start, end, placeholder)
+function plotGraph(from, till, placeholder)
 {
   $.plot(placeholder,[                    
     {
-      data: graph_data ,				//data
-      lines: { show: true, fill: true }		//style
+      data: graphData,
+      lines: { show: true, fill: true }
     }], {
-    xaxis: {  mode: "time", min: ((start)), max: ((end)) },
-    selection: { mode: "xy" }
+      xaxis: { mode: 'time' },
+      selection: { mode: 'xy' }
     }); 
   $('#loading').hide();
 };
 
-//--------------------------------------------------------------------------------------
-// Fetch Data
-//--------------------------------------------------------------------------------------
-function vis_feed_data(api_key,feed_id,from,till, placeholder)
+function fetchDataGraph(apiKey, feedID, from, till, placeholder)
 {
  $('#loading').show();
- $("#stat").html("Loading...  please wait about 5s");
- $.ajax({                                       //Using JQuery and AJAX
+ $('#stat').html('Loading...  please wait about 5s');
+ $.ajax({ 
    url: '/api',                         
-   data: "api_read_token="+api_key+"&feed_id="+feed_id+"&from="+from+"&till=" + till,
-   dataType: 'json',                            //and passes it through as a JSON    
+   data: 'api_read_token=' + apiKey + '&feed_id=' + feedID + '&from=' + from + '&till=' + till,
+   dataType: 'json',
    success: function(data) 
    {
-
-      if (data!=0){
-       
-        paverage = 0;
-        npoints = 0;
-
-        for (var z in data)                     //for all variables
+      if (data != 0){
+        var pAverage = 0;
+        var nPoints  = 0;
+        for (var z in data)
         {
-          paverage += parseFloat(data[z][1]);
-          npoints++;
+          pAverage += parseFloat(data[z][1]);
+          nPoints++;
         }  
-        var timeB  = Number(data[0][0])/1000.0;
-        var timeA  = Number(data[data.length-1][0])/1000.0;
+        var timeB = Number(data[0][0])/1000.0;
+        var timeA = Number(data[data.length-1][0])/1000.0;
 
-        var timeWindow = (timeB-timeA);
-        var timeWidth = timeWindow / npoints;
+        var timeWindow = (timeB - timeA);
+        var timeWidth  = timeWindow / nPoints;
+        var kwhWindow  = (timeWidth * pAverage) / 3600000;
 
-        var kwhWindow = (timeWidth * paverage)/3600000;
-
-        paverage = paverage / npoints;
-        $("#stat").html((paverage).toFixed(1)+" W | "+(kwhWindow).toFixed(1)+" kWh");
-       
-      } else {
-        $("#stat").html("No data");
-      }
-      graph_data = [];   
-      graph_data = data;
-
-      plotGraph(start, end, placeholder);
+        pAverage = pAverage / nPoints;
+        $('#stat').html((pAverage).toFixed(1)+' W | '+(kwhWindow).toFixed(1)+' kWh');
      
+      } else {
+        $('#stat').html('No data');
+      }
+      graphData = [];   
+      graphData = data;
+
+      plotGraph(from, till, placeholder);
     } 
   });
+};
+
+function zoomGraph(placeholder, apiKey, feedID, factor) {
+  var timeWindow = till - from;
+  var middle     = from + timeWindow / 2;
+  timeWindow     = timeWindow * factor;
+  from           = middle - (timeWindow / 2);
+  till           = middle + (timeWindow / 2);
+  fetchDataGraph(apiKey, feedID, from, till, placeholder); 
+};
+
+function shiftGraph(placeholder, apiKey, feedID, direction) {
+  var timeWindow = till - from;
+  var shiftsize  = timeWindow * 0.2;
+  if (direction == 'right') {
+    from += shiftsize;
+    till += shiftsize;
+  } else {
+    from -= shiftsize;
+    till -= shiftsize;
+  };
+  fetchDataGraph(apiKey, feedID, from, till, placeholder); 
+};
+ 
+function setTimeGraph(element, placeholder, apiKey, feedID) {
+  var time = $(element).attr('time');
+  till     = new Date().getTime();
+  from     = till - (3600000 *24 * time);
+  fetchDataGraph(apiKey, feedID, from, till, placeholder); 
+};
+
+function plotSelectedGraph(placeholder, apiKey, feedID, ranges) {
+  if (ranges.xaxis.to - ranges.xaxis.from < 0.00001) ranges.xaxis.to = ranges.xaxis.from + 0.00001;
+  if (ranges.yaxis.to - ranges.yaxis.from < 0.00001) ranges.yaxis.to = ranges.yaxis.from + 0.00001;
+  from = ranges.xaxis.from - 3600000;
+  till = ranges.xaxis.to - 3600000; 
+  fetchDataGraph(apiKey, feedID, from, till, placeholder);  
 };
