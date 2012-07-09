99f74cde282c5b190c8ebe072baeb5ad29c11e76
dovadi <frank.oxener@gmail.com>
2012-07-09 21:54:10 +0200

    Rescue from invalid byte sequence in UTF-8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!, :set_time_zone
 
+  #TODO cover with some sort of test
+  rescue_from(ArgumentError) {|e| raise e unless e.message == "invalid byte sequence in UTF-8"}
+
   private
 
   def set_time_zone
