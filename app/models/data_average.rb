6a1d5b7d9557cdba5a09c4ddf445e4f5d5b4b28b
Frank Oxener <frank.oxener@gmail.com>
2012-07-27 20:50:49 +0200

    Possible fix for unexpected invocation of table_name= with only numbers during calculating average

--- app/models/data_average.rb
+++ app/models/data_average.rb
@@ -2,16 +2,16 @@ class DataAverage
 
   def self.calculate!(identified_by, timeslot)
     return nil unless timeslot.present?
-    last  = DataStore.from(identified_by, timeslot).last
+    last  = DataStore.from(identified_by.to_i, timeslot).last
     if last
-      value = DataStore.from(identified_by).where('created_at >= ? ',last.created_at).average(:value)
+      value = DataStore.from(identified_by.to_i).where('created_at >= ? ',last.created_at).average(:value)
     else
-      value = DataStore.from(identified_by).average(:value)
+      value = DataStore.from(identified_by.to_i).average(:value)
     end
     unless value.nil?
       # Force assigning created_at to prevent stange error with MySql: Mysql2::Error: Column 'created_at' cannot be null
       ds = DataStore.create!(:value => value, :identified_by => identified_by, :timeslot => timeslot, :created_at => Time.now.utc)
-      calculate!(identified_by, Calculator.next(timeslot, ds.created_at))
+      calculate!(identified_by.to_i, Calculator.next(timeslot, ds.created_at))
     end
   end
 
