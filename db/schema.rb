664410019842b4c46d67e3301b319620d841d0be
Frank Oxener <frank.oxener@gmail.com>
2012-07-27 13:02:14 +0200

    Fix devise migration after update to latest version in order to run specs on travis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111225174916) do
+ActiveRecord::Schema.define(:version => 20120708132740) do
 
   create_table "data_stores", :force => true do |t|
     t.float    "value"
@@ -54,24 +54,33 @@ ActiveRecord::Schema.define(:version => 20111225174916) do
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
+  create_table "resets", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "reason"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "resets", ["user_id"], :name => "index_resets_on_user_id"
+
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "",    :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",                       :default => 0
+    t.integer  "failed_attempts",        :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "authentication_token"
-    t.datetime "created_at",                                               :null => false
-    t.datetime "updated_at",                                               :null => false
-    t.boolean  "admin",                                 :default => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
     t.string   "api_read_token"
     t.string   "time_zone"
   end
@@ -81,14 +90,5 @@ ActiveRecord::Schema.define(:version => 20111225174916) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
-
-  create_table "resets", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "reason"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "resets", ["user_id"], :name => "index_resets_on_user_id"
 
 end
