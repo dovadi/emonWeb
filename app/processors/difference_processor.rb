89c66b23ab629077401cdc0150c1be232e55ad37
Frank Oxener <frank.oxener@gmail.com>
2012-10-08 15:20:16 +0200

    Fix issues with difference processor

--- app/processors/difference_processor.rb
+++ app/processors/difference_processor.rb
@@ -10,11 +10,11 @@ class DifferenceProcessor < Processor
 
   def perform
     feed = Feed.find(@argument)
-    last_value = feed.last_value
+    last_value = feed.last_value ? feed.last_value : 0
     difference = (@value - last_value).round(3)
     unless difference == 0
       DataStore.create(:value => difference, :identified_by => @argument)
-      Feed.update(@argument, :last_value => difference)
+      Feed.update(@argument, :last_value => @value)
     end
     difference
   end
--- spec/processors/difference_processor_spec.rb
+++ spec/processors/difference_processor_spec.rb
@@ -12,19 +12,28 @@ describe DifferenceProcessor do
       @feed = Feed.create!(:last_value => 252.55, :name => 'electra', :input_id => 3, :user_id => 1)
     end
 
-    it 'should only store the value is different than last one' do   
-      DataStore.expects(:create).with(:value => 0.21, :identified_by => @feed.id)
-      Feed.expects(:update).with(@feed.id, :last_value => 0.21)
+    it 'should only store the value is different than last one' do
+      Feed.expects(:update).with(@feed.id, :last_value => 252.76)
       processor = DifferenceProcessor.new(252.76, @feed.id)
       processor.perform.should == 0.21
     end
 
-    it 'should not store the value it is the same as the last one' do   
+    it 'should not store the value it is the same as the last one' do
       DataStore.expects(:create).never
       Feed.expects(:update).never
       processor = DifferenceProcessor.new(252.55, @feed.id)
       processor.perform.should == 0.0
     end
+
+    it 'should store the value for the very first time' do
+      @feed.destroy
+      @feed = Feed.create!(:last_value => nil, :name => 'electra', :input_id => 3, :user_id => 1)
+      DataStore.expects(:create).with(:value => 252.76, :identified_by => @feed.id)
+      Feed.expects(:update).with(@feed.id, :last_value => 252.76)
+      processor = DifferenceProcessor.new(252.76, @feed.id)
+      processor.perform.should == 252.76
+    end
+
   end
 
 end
