e4e4c64d8fa8d8bea76710e825b9d4dd8652cfec
Frank Oxener <frank.oxener@gmail.com>
2012-10-28 16:08:08 +0100

    Little improvements on DataStoreCreator spec

--- spec/models/data_store_creator_spec.rb
+++ spec/models/data_store_creator_spec.rb
@@ -18,8 +18,8 @@ describe DataStoreCreator do
     it 'should have timeslots' do
       @creator.timeslots.should == [:one_min, :five_mins, :fifteen_mins, :one_hour, :four_hours, :twelve_hours]
     end
-    describe 'create all tables' do
-      it 'should create the correct tables' do
+    describe 'create table' do
+      it 'should create all the correct tables' do
         @creator.expects(:create_table).with('data_store_12345')
         @creator.expects(:create_table).with('data_store_12345_one_min')
         @creator.expects(:create_table).with('data_store_12345_five_mins')
@@ -29,8 +29,6 @@ describe DataStoreCreator do
         @creator.expects(:create_table).with('data_store_12345_twelve_hours')
         @creator.create_all_tables
       end
-    end
-    describe 'create table' do
       it 'should create a table if not exists' do
         @creator.stubs(:timeslots).returns([])
         @creator.expects(:table_exist?).with('data_store_12345').returns(false)
@@ -43,8 +41,7 @@ describe DataStoreCreator do
 
   describe 'DataStoreCreator with undefined database adapter' do
     before(:each) do
-      db_connector = mock
-      db_connector.stubs(:adapter_name).returns('undefined')
+      db_connector = mock(:adapter_name =>'undefined')
       @creator = DataStoreCreator.new(12345)
       @creator.stubs(:db_connection).returns(db_connector)
     end
@@ -57,8 +54,7 @@ describe DataStoreCreator do
 
   describe 'DataStoreCreator for PostgreSQL' do
     before(:each) do
-      @db_connector = mock
-      @db_connector.stubs(:adapter_name).returns('PostgreSQL')
+      @db_connector = mock(:adapter_name => 'PostgreSQL')
       @creator = DataStoreCreator.new(12345)
       @creator.stubs(:db_connection).returns(@db_connector)
     end
@@ -72,18 +68,18 @@ describe DataStoreCreator do
 
   describe 'DataStoreCreator for Mysql2' do
     before(:each) do
-      @db_connector = mock
-      @db_connector.stubs(:adapter_name).returns('Mysql2')
+      @db_connector = mock(:adapter_name => 'Mysql2')
       @creator = DataStoreCreator.new(12345)
       @creator.stubs(:db_connection).returns(@db_connector)
     end
-    it 'should initialize a DataStorePostgresCreator' do
+    it 'should initialize a DataStoreMysqlCreator' do
       mysql = mock
       mysql.expects(:create_all_tables)
       DataStoreMysqlCreator.expects(:new).with(12345).returns(mysql)
       @creator.execute!
     end
   end
+
 end
 
 describe DataStoreMysqlCreator do
@@ -96,19 +92,21 @@ describe DataStoreMysqlCreator do
 end
 
 describe DataStorePostgresCreator do
-
-  it 'should return the correct sql statement' do
-    @creator = DataStorePostgresCreator.new(12345)
-    @creator.sql_statement('data_store').include?("CREATE SEQUENCE data_store_id_seq").should == true
-  end
-
-  it 'should create a table ' do
+  before(:each) do
     @db_connector = mock
     @db_connector.stubs(:adapter_name).returns('PostgreSQL')
     @creator = DataStorePostgresCreator.new(12345)
-    @creator.expects(:table_exist?).with('data_store_12345').returns(false)
     @creator.stubs(:db_connection).returns(@db_connector)
+  end
+
+  it 'should return the correct sql statement' do
+    @creator.sql_statement('data_store').include?("CREATE SEQUENCE data_store_id_seq").should == true
+  end
+
+  it 'should create a table with a ROLLBACK first' do
+    @creator.expects(:table_exist?).with('data_store_12345').returns(false)
     @creator.expects(:sql_statement).with('data_store_12345').returns('sql')
+
     @db_connector.expects(:execute).with('ROLLBACK')
     @db_connector.expects(:execute).with('sql')
     @creator.create_table('data_store_12345')
